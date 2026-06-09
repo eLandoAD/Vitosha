b187f55deca9e324fba637ae6f4f7ff2e28858db
eduard.apetroaie <eduard.apetroaie@marconirovereto.it>
2026-06-09 15:28:01 +0300

    feat: Implement forgot and reset password functionality with corresponding routes and components

--- Vitosha-eLando-Project/src/app/app.routes.ts
+++ Vitosha-eLando-Project/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { Register } from './pages/register/register';
 import { VerifyEmail } from './pages/verify-email/verify-email';
 import { authGuard } from './guards/auth-guard';
 import { Dashboard } from './pages/dashboard/dashboard';
+import { ForgotPassword } from './pages/forgot-password/forgot-password';
+import { ResetPassword } from './pages/reset-password/reset-password';
 
 
 
@@ -12,6 +14,8 @@ export const routes: Routes = [
     { path: 'login', component: Login },
     { path: 'register', component: Register },
     { path: 'verify-email', component: VerifyEmail },
+    { path: 'forgot-password', component: ForgotPassword },
+    { path: 'reset-password', component: ResetPassword },
     { path: 'dashboard', component: Dashboard, canActivate: [authGuard] }
 ];
 
